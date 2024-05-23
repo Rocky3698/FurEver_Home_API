825b757934af7a0e2f096694bbb078768a8710d5
Rocky Chowdhury <rocky20809@gmail.com>
2024-05-23 18:39:54 +0600

    host seted

--- FurEver_Home/settings.py
+++ FurEver_Home/settings.py
@@ -28,13 +28,13 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
 SESSION_COOKIE_SAMESITE = 'Lax'
 CSRF_COOKIE_SAMESITE = 'Lax'
-
+CSRF_TRUSTED_ORIGINS = ['https://furever-home-api.onrender.com']
 # Application definition
 
 INSTALLED_APPS = [
